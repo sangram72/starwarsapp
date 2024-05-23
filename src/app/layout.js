5f1488d962dc0ffa5f96c769e0a47e799c2616f2
Sangram Nandi <67709894+sangram72@users.noreply.github.com>
2024-05-23 15:52:24 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className} style={{backgroundImage: `url("./back.jpg")`}}>
+      <body className={inter.className} style={{backgroundImage: `url("./star.jpg")`}}>
         <Suspense>
       <ChakraProvider>{children}</ChakraProvider>
       </Suspense>
